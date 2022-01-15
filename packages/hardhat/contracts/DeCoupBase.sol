4842f3b657e2a06774987c89bdc11540810a9fa0
AIRESH BHAT <nelliairesh@gmail.com>
2022-01-15 17:46:05 +0530

    Start defining data types

--- packages/hardhat/contracts/DeCoupBase.sol
+++ packages/hardhat/contracts/DeCoupBase.sol
@@ -11,5 +11,28 @@ contract DeCoupBase is AccessControlContract {
     /*** EVENTS ***/
 
     /// @dev The MintCoupon event is fired whenever a new coupon comes into existence.
-    event Mint(address owner, uint256 couponId, );
+    event Mint(address owner, uint256 couponId);
+
+    /// @dev Transfer event as defined in current draft of ERC721. Emitted every time a coupon
+    ///  ownership is assigned, including creations.
+    event Transfer(address from, address to, uint256 tokenId);
+
+    struct Coupon {
+        // Coupon design address
+        string couponDesignURL;
+
+        // The creator of the coupon.
+        address generator;
+
+        // The approver of the coupon. The address giving the offer.
+        address sink;
+
+        // The minimum timestamp period for which the coupons is valid. Starts from when the coupon
+        // is transferred from the generator to the user. i.e. Time from when coupon validity starts to
+        // coupon validity + couponeValidtyTime
+        uint64 couponValidityTime;
+
+        // Coupon Type
+        uint8 couponType;
+    }
 }
