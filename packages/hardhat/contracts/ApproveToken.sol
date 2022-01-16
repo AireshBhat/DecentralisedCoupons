480be444d4314174b46db4ee548cd18b7f4e3db7
Ravi Bharadwaj C <f20160244@hyderabad.bits-pilani.ac.in>
2022-01-16 16:11:39 +0530

    add validations to approval

--- packages/hardhat/contracts/ApproveToken.sol
+++ packages/hardhat/contracts/ApproveToken.sol
@@ -6,6 +6,16 @@ import "./CouponHelper.sol";
 contract ApproveToken is CouponHelper {
     function approveToken(uint256 _couponId) internal {
         require(coupons[_couponId].isCouponValid == false);
+
+        // check if sender is a valid sink
+        require(sinks[msg.sender] == true);
+
+        // check if sender is the assigned sink of the token
+        require(coupons[_couponId].sink == msg.sender);
+
+        // check if the current owner is the generatory; can only be approved when the owner is still the generator
+        require(generators[coupons[_couponId].owner] == true);
+
         coupons[_couponId].isCouponValid = true;
         coupons[_couponId].couponCreationTime = uint64(block.timestamp);
         emit Approve(coupons[_couponId].owner, _couponId);
